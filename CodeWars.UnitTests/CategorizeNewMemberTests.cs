5a33ec761f07bd716fc0de88145f32db0d739eeb
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-05-07 12:10:33 +0200

    Categorize New Member

---
+++ CodeWars.UnitTests/CategorizeNewMemberTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.UnitTests
+{
+    public class CategorizeNewMemberTests
+    {
+        [Fact]
+        public void SampleTest()
+        {
+            Assert.Equal(new[] { "Open", "Senior", "Open", "Senior" }, 
+                CategorizeNewMember.OpenOrSenior(
+                    new[] { new[] { 45, 12 }, new[] { 55, 21 }, new[] { 19, 2 }, new[] { 104, 20 } }
+                    )
+                );
+
+            Assert.Equal(new[] { "Open", "Open", "Open", "Open" }, 
+                CategorizeNewMember.OpenOrSenior(
+                    new[] { new[] { 3, 12 }, new[] { 55, 1 }, new[] { 91, -2 }, new[] { 54, 23 } }
+                    )
+                );
+
+            Assert.Equal(new[] { "Senior", "Open", "Open", "Open" }, 
+                CategorizeNewMember.OpenOrSenior(
+                    new[] { new[] { 59, 12 }, new[] { 45, 21 }, new[] { -12, -2 }, new[] { 12, 12 } }
+                    )
+                );
+
+            Assert.Equal(new[] { "Senior", "Open", "Open", "Open" },
+                CategorizeNewMember.OpenOrSenior(
+                    new[] { new[] { 55, 8 }, new[] { 56, 7 }, new[] { 80, 2 }, new[] { 30, 12 } }
+                    )
+                );
+        }
+    }
+}
---
+++ CodeWars/CategorizeNewMember.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Core
+{
+    public static class CategorizeNewMember
+    {
+        public static IEnumerable<string> OpenOrSenior(int[][] data)
+        {
+            foreach (var member in data)
+            {
+                if (55 <= member[0] && 7 < member[1])
+                    yield return "Senior";
+                else
+                    yield return "Open";
+            }
+        }
+    }
+}
