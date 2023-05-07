dda2ef5c52ce81b0c535a3ee310ce650c8457d88
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-05-07 11:54:23 +0200

    Sort array by string length

---
+++ CodeWars/SortArrayByStringLength.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Core
+{
+    public static class SortArrayByStringLength
+    {
+        public static string[] SortByLength(string[] array)
+        {
+            return array.OrderBy(e => e.Length).ToArray();
+        }
+    }
+}
