53c7d001bf87cc14dd7bc3c73bd1c5df32162d43
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-05-07 11:44:36 +0200

    Convert boolean values to strings 'Yes' or 'No'.

---
+++ CodeWars/BoolToString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Core
+{
+    public static class BoolToString
+    {
+        public static string boolToWord(bool word)
+        {
+            return word ? "Yes" : "No";
+        }
+    }
+}
