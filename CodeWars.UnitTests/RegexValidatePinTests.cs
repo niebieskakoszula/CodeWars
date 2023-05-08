b87c9c19a0fcf2cfc4a84c8d16c82f9236052957
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-05-08 12:17:05 +0200

    Regex validate PIN code

---
+++ CodeWars.UnitTests/RegexValidatePinTests.cs
@@ -0,0 +1,60 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.UnitTests
+{
+    public class RegexValidatePinTests
+    {
+
+        [Theory, Description("ValidatePin should return false for pins with length other than 4 or 6")]
+        [InlineData("1")]        
+        [InlineData("12")]
+        [InlineData("123")]
+        [InlineData("12345")]
+        [InlineData("1234567")]
+        [InlineData("-1234")]
+        [InlineData("1.234")]
+        [InlineData("00000000")]
+        public void ValidatePin_GivenWrongLenght_ReturnsFalse(string pin)
+        {
+            RegexValidatePin.ValidatePin(pin).Should().BeFalse();
+        }
+
+        [Theory, Description("ValidatePin should return false for pins which contain characters other than digits")]
+        [InlineData("a234")]
+        [InlineData(".234")]
+        [InlineData("0.234")]
+        public void ValidatePin_GivenNotANumber_ReturnsFalse(string pin)
+        {
+            RegexValidatePin.ValidatePin(pin).Should().BeFalse();
+        }
+
+        [Theory, Description("ValidatePin should return true for valid pins")]
+        [InlineData("1234")]
+        [InlineData("0000")]
+        [InlineData("1111")]
+        [InlineData("123456")]
+        [InlineData("098765")]
+        [InlineData("000000")]
+        [InlineData("090909")]
+        public void ValidatePin_GivenValidPin_ReturnsTrue(string pin)
+        {
+            RegexValidatePin.ValidatePin(pin).Should().BeTrue();
+        }
+
+        [Theory, Description("ValidatePin should return false for pins which contain white spaces")]
+        [InlineData("12 34")]
+        [InlineData("12\n34")]
+        [InlineData("023\t4")]
+        public void ValidatePin_GivenWhiteSpace_ReturnsFalse(string pin)
+        {
+            RegexValidatePin.ValidatePin(pin).Should().BeFalse();
+        }
+    }
+}
---
+++ CodeWars/RegexValidatePin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CodeWars.Core
+{
+    public static class RegexValidatePin
+    {
+        static readonly string pattern = @"\A(\d{4}|\d{6})\z";
+        public static bool ValidatePin(string pin)
+        {
+            return new Regex(pattern).IsMatch(pin);
+        }
+    }
+}
