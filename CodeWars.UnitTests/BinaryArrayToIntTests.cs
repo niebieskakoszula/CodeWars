c4160c55cc15b272969199dce941dfaa8eafb3aa
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-05-08 12:16:27 +0200

    Ones and Zeros

---
+++ CodeWars.UnitTests/BinaryArrayToIntTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.UnitTests
+{
+    public class BinaryArrayToIntTests
+    {
+        [Fact]
+        public void BinaryArrayToNumber_GivenEmptyArray_ReturnsZero()
+        {
+            BinaryArrayToInt.BinaryArrayToNumber(new int[] { }).Should().Be(0);
+        }
+        [Fact]
+        public void BinaryArrayToNumber_GivenArrayWithOneElement_ReturnsThatElement()
+        {
+            BinaryArrayToInt.BinaryArrayToNumber(new int[] { 0 }).Should().Be(0);
+            BinaryArrayToInt.BinaryArrayToNumber(new int[] { 1 }).Should().Be(1);
+        }
+        [Fact]
+        public void BinaryArrayToNumber_GivenArrayWithManyElements_ReturnsDecimalValue()
+        {
+            BinaryArrayToInt.BinaryArrayToNumber(new int[] { 0, 0 }).Should().Be(0);
+            BinaryArrayToInt.BinaryArrayToNumber(new int[] { 0, 1 }).Should().Be(1);
+            BinaryArrayToInt.BinaryArrayToNumber(new int[] { 1, 0 }).Should().Be(2);
+            BinaryArrayToInt.BinaryArrayToNumber(new int[] { 1, 1 }).Should().Be(3);
+            BinaryArrayToInt.BinaryArrayToNumber(new int[] { 1, 0, 0 }).Should().Be(4);
+            BinaryArrayToInt.BinaryArrayToNumber(new int[] { 1, 0, 1 }).Should().Be(5);
+            BinaryArrayToInt.BinaryArrayToNumber(new int[] { 1, 1, 0 }).Should().Be(6);
+            BinaryArrayToInt.BinaryArrayToNumber(new int[] { 1, 1, 1 }).Should().Be(7);
+            BinaryArrayToInt.BinaryArrayToNumber(new int[] { 0, 0, 0 }).Should().Be(0);
+            BinaryArrayToInt.BinaryArrayToNumber(new int[] { 1, 1, 1, 1 }).Should().Be(15);
+            BinaryArrayToInt.BinaryArrayToNumber(new int[] { 1, 1, 1, 1, 1 }).Should().Be(31);
+            BinaryArrayToInt.BinaryArrayToNumber(new int[] { 1, 1, 1, 1, 1, 1 }).Should().Be(63);
+            BinaryArrayToInt.BinaryArrayToNumber(new int[] { 1, 1, 1, 1, 1, 1, 1 }).Should().Be(127);
+            BinaryArrayToInt.BinaryArrayToNumber(new int[] { 1, 1, 1, 1, 1, 1, 1, 1 }).Should().Be(255);
+        }
+    }
+}
---
+++ CodeWars/BinaryArrayToInt.cs
@@ -0,0 +1,12 @@
+﻿//https://www.codewars.com/kata/578553c3a1b8d5c40300037c/csharp
+
+namespace CodeWars.Core
+{
+    public class BinaryArrayToInt
+    {
+        public static int BinaryArrayToNumber(int[] binaryArray)
+        {
+            return binaryArray.Aggregate(0, (acc, x) => acc * 2 + x);
+        }
+    }
+}
