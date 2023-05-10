1f354d88b91897b6cdabd503b5ba8039bc156150
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-05-10 14:43:11 +0200

    Create Phone Number

---
+++ CodeWars.UnitTests/CreatePhoneNumberTests.cs
@@ -0,0 +1,12 @@
+﻿namespace CodeWars.UnitTests
+{
+    public class CreatePhoneNumberTests
+    {
+        [Fact]
+        public void CreatePhoneNumberTest()
+        {
+            Assert.Equal("(123) 456-7890", CreatePhoneNumber.CreatePhoneNumberMethod(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }));
+            Assert.Equal("(111) 111-1111", CreatePhoneNumber.CreatePhoneNumberMethod(new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }));
+        }
+    }
+}
---
+++ CodeWars/CreatePhoneNumber.cs
@@ -0,0 +1,14 @@
+﻿//https://www.codewars.com/kata/525f50e3b73515a6db000b83/csharp
+//requires LINQ
+
+namespace CodeWars.Core
+{
+    public class CreatePhoneNumber
+    {
+        public static string CreatePhoneNumberMethod(int[] numbers)
+        {
+            return string.Format(format: "({0}{1}{2}) {3}{4}{5}-{6}{7}{8}{9}", 
+                numbers.Cast<object>().ToArray());
+        }
+    }
+}
