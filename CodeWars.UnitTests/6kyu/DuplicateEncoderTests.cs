7dfdba923c36e0bacb9a65cb923e80fb004d7e4b
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-05-10 17:01:58 +0200

    DuplicateEncoder

---
+++ CodeWars.UnitTests/6kyu/DuplicateEncoderTests.cs
@@ -0,0 +1,14 @@
+﻿namespace CodeWars.UnitTests._6kyu;
+
+public class DuplicateEncoderTests
+{
+    [Theory]
+    [InlineData("din", "(((")]
+    [InlineData("recede", "()()()")]
+    [InlineData("Success", ")())())")]
+    [InlineData("(( @", "))((")]
+    public void BasicTest1(string input, string expected)
+    {
+        Assert.Equal(expected, DuplicateEncoder.DuplicateEncode(input));
+    }
+}
---
+++ CodeWars/6kyu/DuplicateEncoder.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace CodeWars.Core._6kyu
+{
+    public class DuplicateEncoder
+    {
+        public static string DuplicateEncode(string word)
+        {
+            var result = new StringBuilder();
+            word = word.ToLower();
+            var countDictionary = word.GroupBy(x => x)
+                .Select(x => new { x.Key, Count = x.Count() })
+                .ToDictionary(x =>x.Key, x => x.Count);
+            
+            foreach(char c in word)
+            {
+                if (countDictionary[c] > 1)
+                    result.Append(')');
+                else
+                    result.Append('(');
+            }
+
+            return result.ToString();
+        }
+    }
+}
