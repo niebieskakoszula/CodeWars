edbc8d86c49e76ce92956bc0850f502b2fe8c19c
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-05-10 15:05:41 +0200

    StopgninnipSMysdroW

---
+++ CodeWars/StopgninnipSMysdroW.cs
@@ -0,0 +1,14 @@
+﻿namespace CodeWars.Core
+{
+    public class StopgninnipSMysdroW
+    {
+        public static string SpinWords(string sentence)
+        {
+            var words = sentence.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+                if (words[i].Length >= 5)
+                    words[i] = new string(words[i].Reverse().ToArray());
+            return string.Join(" ", words);
+        }
+    }
+}
