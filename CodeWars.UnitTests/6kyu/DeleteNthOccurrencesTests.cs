e272e65cf398c77daff767696581847ab7a59837
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-05-11 13:13:11 +0200

    DeleteNthOccurrences

---
+++ CodeWars.UnitTests/6kyu/DeleteNthOccurrencesTests.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeWars.UnitTests._6kyu
+{
+    public class DeleteNthOccurrencesTests
+    {
+        [Theory]
+        [InlineData(new int[] {21, 37, 21, 22}, 1, new int[] { 21, 37, 22 })]
+        [InlineData(new int[] {1, 1, 3, 3, 7, 2, 2, 2, 2}, 3, new int[] { 1, 1, 3, 3, 7, 2, 2, 2 })]
+        public void DeleteNthOccurrencesTest(int[] input, int x, int[] expected)
+        {
+            Assert.Equal(expected, DeleteNthOccurrences.DeleteNth(input, x));
+        }
+    }
+}
---
+++ CodeWars/6kyu/DeleteNthOccurrences.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeWars.Core._6kyu
+{
+    public static class DeleteNthOccurrences
+    {
+        public static int[] DeleteNth(int[] arr, int x)
+        {
+            var result = new List<int>();
+            var countDictionary = arr.GroupBy(x => x)
+                .ToDictionary(x => x.Key, x => 0);
+
+            foreach (var item in arr)
+            {
+                if (countDictionary[item] < x) 
+                { 
+                    result.Add(item);
+                    countDictionary[item]++; 
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
