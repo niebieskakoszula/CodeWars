bd15c3bb1b3b2de94e5f04c4810c67381b004c69
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-05-11 13:24:06 +0200

    HighestScoringWord

---
+++ CodeWars.UnitTests/6kyu/HighestScoringWordTests.cs
@@ -0,0 +1,28 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CodeWars.UnitTests._6kyu
+{
+    public class HighestScoringWordTests
+    {
+        [Theory]
+        [MemberData(nameof(HighestScoringWordTestsCases))]
+        public void HighTest(string input, string expected)
+        {
+            Assert.Equal(expected, HighestScoringWord.High(input));
+        }
+        public static IEnumerable<object[]> HighestScoringWordTestsCases
+        {
+            get
+            {
+                yield return new object[] { "man i need a taxi up to ubud", "taxi" };
+                yield return new object[] { "what time are we climbing up to the volcano", "volcano" };
+                yield return new object[] { "take me to semynak", "semynak" };
+                yield return new object[] { "aa b", "aa" };
+                yield return new object[] { "b aa", "b" };
+                yield return new object[] { "bb d", "bb" };
+                yield return new object[] { "d bb", "d" };
+                yield return new object[] { "aaa b", "aaa" };
+            }
+        }
+    }
+}
---
+++ CodeWars/6kyu/HighestScoringWord.cs
@@ -0,0 +1,26 @@
+﻿namespace CodeWars.Core._6kyu
+{
+    public static class HighestScoringWord
+    {
+        public static string High(string s)
+        {
+            var words = s.Split(' ');
+            var maxScore = 0;
+            var maxScoreWord = "";
+            foreach(var word in words)
+            {
+                var score = 0;
+                foreach(char c in word)
+                {
+                    score += c - 'a' + 1;
+                }
+                if (score > maxScore)
+                {
+                    maxScore = score;
+                    maxScoreWord = word;
+                }
+            }
+            return maxScoreWord;
+        }
+    }
+}
