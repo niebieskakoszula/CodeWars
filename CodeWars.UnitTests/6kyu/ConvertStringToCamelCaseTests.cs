d28ed180d3f7bf38e835221c3f244698b17b255a
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-05-11 13:35:11 +0200

    ConvertStringToCamelCase

---
+++ CodeWars.UnitTests/6kyu/ConvertStringToCamelCaseTests.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeWars.UnitTests._6kyu
+{
+    public class ConvertStringToCamelCaseTests
+    {
+        [Theory]
+        [InlineData("the-stealth-warrior", "theStealthWarrior")]
+        [InlineData("The_Stealth_Warrior", "TheStealthWarrior")]
+        public void ToCamelCase_DontChangeFirstLetter(string input, string expected)
+        {
+            Assert.Equal(expected, ConvertStringToCamelCase.ToCamelCase(input));
+        }
+        [Theory]
+        [InlineData("the_stealth-warrior", "theStealthWarrior")]
+        [InlineData("The_Stealth-Warrior", "TheStealthWarrior")]
+        public void ToCamelCase_WorksWithMixedDashesAndUnderScores(string input, string expected)
+        {
+            Assert.Equal(expected, ConvertStringToCamelCase.ToCamelCase(input));
+        }
+    }
+}
---
+++ CodeWars/6kyu/ConvertStringToCamelCase.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace CodeWars.Core._6kyu
+{
+    public static class ConvertStringToCamelCase
+    {
+        public static string ToCamelCase(string str)
+        {
+            var result = new StringBuilder();
+            var words = str.Split(new char[] { '-', '_' });
+            result.Append(words[0]);
+            for (int i = 1; i < words.Length; i++)
+            {
+                result.Append(char.ToUpper(words[i][0]));
+                result.Append(words[i].Substring(1));
+            }
+            return result.ToString();
+        }
+    }
+}
