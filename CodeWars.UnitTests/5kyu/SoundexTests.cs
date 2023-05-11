ce3c7686d37bf51565c6975de022278140e6b542
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-05-11 15:34:42 +0200

    Soundex

---
+++ CodeWars.UnitTests/5kyu/SoundexTests.cs
@@ -0,0 +1,53 @@
+﻿namespace CodeWars.UnitTests._5kyu
+{
+    public class SoundexTests
+    {
+        [Theory]
+        [InlineData("A", "A000")]
+        [InlineData("Ab", "A100")]
+        [InlineData("Ax", "A200")]
+        [InlineData("Ashcraft", "A261")]
+        [InlineData("hjaidjaqmfkd", "H232")]
+        [InlineData("uryrtkzp", "U663")]
+        public void SoundexOneWord(string input, string expected)
+        {
+            Assert.Equal(expected, Soundex.Encode(input));
+        }
+
+        [Theory]
+        [InlineData("Hello World", "H400 W643")]
+        public void SoundexMultipleWords(string input, string expected)
+        {
+            Assert.Equal(expected, Soundex.Encode(input));
+        }
+
+        [Theory]
+        [InlineData("Sarah Connor", "S600 C560")]
+        [InlineData("sarah connor", "S600 C560")]
+        [InlineData("Sayra Cunnarr", "S600 C560")]
+        [InlineData("Serah Coner", "S600 C560")]
+        [InlineData("Sarh Connor", "S600 C560")]
+        [InlineData("Arnold Schwarzenegger", "A654 S625")] //Shame it's not T800 lol
+        public void SoundexArnieTest(string input, string expected)
+        {
+            Assert.Equal(expected, Soundex.Encode(input));
+        }
+
+        [Theory]
+        [InlineData("Tim", "T500")]
+        [InlineData("Joe", "J000")]
+        [InlineData("Ann", "A500")]
+        [InlineData("Madge", "M320")]
+        public void SoundexShortNames(string input, string expected)
+        {
+            Assert.Equal(expected, Soundex.Encode(input));
+        }
+
+        [Theory]
+        [InlineData("uryrtkzp", "U663")]
+        public void SoundexStepsOrder(string input, string expected)
+        {
+            Assert.Equal(expected, Soundex.Encode(input));
+        }
+    }
+}
---
+++ CodeWars/5kyu/Soundex.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+
+namespace CodeWars.Core._5kyu
+{
+    public static class Soundex
+    {
+        public static string Encode(string names)
+        {
+            var result = new StringBuilder();
+            foreach (var name in names.Split(' '))
+            {
+                result.Append(EncodeWord(name));
+                result.Append(' ');
+            }
+            return result.ToString().TrimEnd();
+        }
+        public static string EncodeWord(string word)
+        {
+            var result = new StringBuilder();
+            result.Append(char.ToUpper(word[0])); //Should be mentioned in description
+            word = word.ToLower();
+            var previousCode = GetCode(word[0]);
+            for (int i = 1; i < word.Length; i++)
+            {
+                var code = GetCode(word[i]);
+                if (code == '0')
+                    continue;
+                else if (code == '-')
+                    previousCode = code;
+                else if (code != previousCode)
+                {
+                    result.Append(code);
+                    previousCode = code;
+                }
+            }
+            return result.ToString().PadRight(4, '0')[..4];
+        }
+        private static char GetCode(char c)
+        {
+            return c switch
+            {
+                'b' or 'f' or 'p' or 'v' => '1',
+                'c' or 'g' or 'j' or 'k' or 'q' or 's' or 'x' or 'z' => '2',
+                'd' or 't' => '3',
+                'l' => '4',
+                'm' or 'n' => '5',
+                'r' => '6',
+                'a' or 'e' or 'i' or 'o' or 'u' or 'y' => '-', //those letters should be removed AFTER replacing lettes with numbers
+                                                               //so I added '-' to make sure I won't skip them while removing adjacent duplicates
+                _ => '0',
+            };
+        }
+    }
+}
