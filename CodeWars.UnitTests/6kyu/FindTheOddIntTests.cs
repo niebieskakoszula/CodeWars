c8c6bec72f05a1216d82dee93e45dbd1bc675038
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-07-17 01:16:34 +0200

    FindTheOddInt

---
+++ CodeWars.UnitTests/6kyu/FindTheOddIntTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.UnitTests._6kyu
+{
+    public class FindTheOddIntTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 2, 3, 3 }, 1)]
+        [InlineData(new int[] { 1, 1, 2, 2, 3, 3, 4 }, 4)]
+        [InlineData(new int[] { 7 }, 7)]
+        [InlineData(new int[] { 0 }, 0)]
+        [InlineData(new int[] { 1, 1, 2 }, 2)]
+        [InlineData(new int[] { 0, 1, 0, 1, 0 }, 0)]
+        [InlineData(new int[] { 1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1 }, 4)]
+        [InlineData(new int[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 }, 5)]
+        public void FindOddTest(int[] input, int expected)
+        {
+            Assert.Equal(expected, FindTheOddInt.FindOdd(input));
+        }
+    }
+}
---
+++ CodeWars/6kyu/FindTheOddInt.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeWars.Core._6kyu
+{
+    public class FindTheOddInt
+    {
+        public static int FindOdd(int[] seq)
+        {
+            var dict = new Dictionary<int, int>();
+            foreach (var item in seq)
+            {
+                if (dict.ContainsKey(item))
+                    dict[item]++;
+                else
+                    dict.Add(item, 1);
+            }
+            foreach (var item in dict)
+            {
+                if (item.Value % 2 != 0)
+                    return item.Key;
+            }
+            return 0;
+        }
+    }
+}
