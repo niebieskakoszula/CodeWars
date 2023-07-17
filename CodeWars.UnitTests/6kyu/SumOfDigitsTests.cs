914fe29524972ef5e839e8161ce8e0dff427e5fb
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-07-17 20:16:39 +0200

    SumOfDigits

---
+++ CodeWars.UnitTests/6kyu/SumOfDigitsTests.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeWars.UnitTests._6kyu
+{
+    public class SumOfDigitsTests
+    {
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(123, 6)]
+        [InlineData(1234, 1)]
+        [InlineData(12345, 6)]
+        [InlineData(16, 7)]
+        [InlineData(942, 6)]
+        [InlineData(132189, 6)]
+        [InlineData(493193, 2)]
+        [InlineData(999999999999, 9)]
+        public void DigitalRootTest(long n, int expected)
+        {
+            Assert.Equal(expected, SumOfDigits.DigitalRoot(n));
+        }
+    }
+}
---
+++ CodeWars/6kyu/SumOfDigits.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeWars.Core._6kyu
+{
+    public class SumOfDigits
+    {
+        public static int DigitalRoot(long n)
+        {
+            var result = n.ToString().Select(x => int.Parse(x.ToString())).Sum();
+            return result < 10 ? result : DigitalRoot(result);
+        }
+    }
+}
