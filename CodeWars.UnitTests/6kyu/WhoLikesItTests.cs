621dd65d4ea6287b9be9b0d6973a5a288055ce98
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-07-17 13:34:22 +0200

    WhoLikesIt

---
+++ CodeWars.UnitTests/6kyu/WhoLikesItTests.cs
@@ -0,0 +1,15 @@
+﻿namespace CodeWars.UnitTests._6kyu
+{
+    public class WhoLikesItTests
+    {
+        [Fact]
+        public void Tests()
+        {
+            Assert.Equal("no one likes this", WhoLikesIt.Solve(new string[0]));
+            Assert.Equal("Peter likes this", WhoLikesIt.Solve(new string[] { "Peter" }));
+            Assert.Equal("Jacob and Alex like this", WhoLikesIt.Solve(new string[] { "Jacob", "Alex" }));
+            Assert.Equal("Max, John and Mark like this", WhoLikesIt.Solve(new string[] { "Max", "John", "Mark" }));
+            Assert.Equal("Alex, Jacob and 2 others like this", WhoLikesIt.Solve(new string[] { "Alex", "Jacob", "Mark", "Max" }));
+        }
+    }
+}
---
+++ CodeWars/6kyu/WhoLikesIt.cs
@@ -0,0 +1,19 @@
+﻿//https://www.codewars.com/kata/5266876b8f4bf2da9b000362/csharp
+
+namespace CodeWars.Core._6kyu
+{
+    public class WhoLikesIt
+    {
+        public static string Solve(string[] names)
+        {
+            switch (names.Length)
+            {
+                case 0: return "no one likes this";
+                case 1: return names[0] + " likes this";
+                case 2: return names[0] + " and " + names[1] + " like this";
+                case 3: return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+                default: return names[0] + ", " + names[1] + " and " + (names.Length - 2).ToString() + " others like this";
+            }
+        }
+    }
+}
