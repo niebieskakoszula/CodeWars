5714c1386ee9d601c75bb37bcff6d9c0d1c5282a
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-07-17 19:44:26 +0200

    HumanReadableTime

---
+++ CodeWars.UnitTests/5kyu/HumanReadableTimeTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.UnitTests._5kyu
+{
+    public class HumanReadableTimeTests
+    {
+        [Theory]
+        [InlineData("00:00:00", 0)]
+        [InlineData("00:00:05", 5)]
+        [InlineData("00:01:00", 60)]
+        [InlineData("23:59:59", 86399)]
+        [InlineData("99:59:59", 359999)]
+        public void Tests(string expected, int seconds)
+        {
+            Assert.Equal(expected, HumanReadableTime.GetReadableTime(seconds));
+        }
+    }
+}
---
+++ CodeWars/5kyu/HumanReadableTime.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeWars.Core._5kyu
+{
+    public class HumanReadableTime
+    {
+        public static string GetReadableTime(int seconds)
+        {
+            var result = TimeSpan.FromSeconds(seconds);
+            return string.Format("{0:00}:{1:00}:{2:00}", (int)result.TotalHours, result.Minutes, result.Seconds);
+        }
+    }
+}
