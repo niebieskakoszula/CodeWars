c298c7171ced89c7c38ed7d8028b41128aaf4355
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-07-18 09:45:03 +0200

    HumanReadableDurationFormat

---
+++ CodeWars.UnitTests/4kyu/HumanReadableDurationFormatTests.cs
@@ -0,0 +1,26 @@
+﻿using CodeWars.Core._4kyu;
+
+namespace CodeWars.UnitTests._4kyu
+{
+    public class HumanReadableDurationFormatTests
+    {
+        [Theory]
+        [InlineData(0, "now")]
+        [InlineData(1, "1 second")]
+        [InlineData(62, "1 minute and 2 seconds")]
+        [InlineData(120, "2 minutes")]
+        [InlineData(3600, "1 hour")]
+        [InlineData(3662, "1 hour, 1 minute and 2 seconds")]
+        [InlineData(15731080, "182 days, 1 hour, 44 minutes and 40 seconds")]
+        [InlineData(132030240, "4 years, 68 days, 3 hours and 4 minutes")]
+        [InlineData(205851834, "6 years, 192 days, 13 hours, 3 minutes and 54 seconds")]
+        [InlineData(253374061, "8 years, 12 days, 13 hours, 41 minutes and 1 second")]
+        [InlineData(242062374, "7 years, 246 days, 15 hours, 32 minutes and 54 seconds")]
+        [InlineData(101956166, "3 years, 85 days, 1 hour, 9 minutes and 26 seconds")]
+        [InlineData(33243586, "1 year, 19 days, 18 hours, 19 minutes and 46 seconds")]
+        public void FormatDuration(int seconds, string expected)
+        {
+            Assert.Equal(expected, HumanReadableDurationFormat.FormatDuration(seconds));
+        }
+    }
+}
---
+++ CodeWars/4kyu/HumanReadableDurationFormat.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace CodeWars.Core._4kyu
+{
+    public class HumanReadableDurationFormat
+    {
+        public static string FormatDuration(int seconds)
+        {
+            if (seconds == 0)
+                return "now";
+            var time = TimeSpan.FromSeconds(seconds);
+            var temp = new List<string>();
+
+            var years = time.Days / 365;
+            if (years > 0)
+                temp.Add($"{years} {(years == 1 ? "year" : "years")}");
+            var days = time.Days % 365;
+            if (days > 0)
+                temp.Add($"{days} {(days == 1 ? "day" : "days")}");
+            if (time.Hours > 0)
+                temp.Add($"{time.Hours} {(time.Hours == 1 ? "hour" : "hours")}");
+            if (time.Minutes > 0)
+                temp.Add($"{time.Minutes} {(time.Minutes == 1 ? "minute" : "minutes")}");
+            if (time.Seconds > 0)
+                temp.Add($"{time.Seconds} {(time.Seconds == 1 ? "second" : "seconds")}");
+
+            StringBuilder result = new StringBuilder();
+            result.Append(temp[0]);
+            for (int i = 1; i < temp.Count; i++)
+            {
+                if (i == temp.Count - 1)
+                    result.Append($" and {temp[i]}");
+                else
+                    result.Append($", {temp[i]}");
+            }
+            return result.ToString();
+        }
+    }
+}
