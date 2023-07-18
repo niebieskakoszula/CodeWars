8c56c91e9269c0c4d4f4731f2235071025e46e4e
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-07-18 17:40:19 +0200

    StripComments

---
+++ CodeWars.UnitTests/4kyu/StripCommentsSolutionTests.cs
@@ -0,0 +1,15 @@
+﻿using CodeWars.Core._4kyu;
+
+namespace CodeWars.UnitTests._4kyu
+{
+    public class StripCommentsSolutionTests
+    {
+        [Theory]
+        [InlineData("apples, pears # and bananas\ngrapes\nbananas !apples", new string[] { "#", "!" }, "apples, pears\ngrapes\nbananas")]
+        [InlineData("a #b\nc\nd $e f g", new string[] { "#", "$" }, "a\nc\nd")]
+        public void StripComments(string text, string[] commentSymbols, string expected)
+        {
+            Assert.Equal(expected, StripCommentsSolution.StripComments(text, commentSymbols));
+        }
+    }
+}
---
+++ CodeWars/4kyu/StripCommentsSolution.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace CodeWars.Core._4kyu
+{
+    public class StripCommentsSolution
+    {
+        public static string StripComments(string text, string[] commentSymbols)
+        {
+            var result = new StringBuilder();
+            var lines = text.Split('\n');
+            for(int i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                var minIndex = line.Length;
+                foreach (var symbol in commentSymbols)
+                {
+                    var index = line.IndexOf(symbol);
+                    if (index != -1)
+                        minIndex = Math.Min(minIndex, index);
+                }
+                result.Append(line.Substring(0, minIndex).TrimEnd() + (i == lines.Length -1 ? "" : "\n"));
+            }
+            return result.ToString();
+        }
+    }
+}
