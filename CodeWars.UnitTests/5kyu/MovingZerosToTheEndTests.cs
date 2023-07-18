dade001b00f1c6d140642d676b253d1c5d7d2cb9
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-07-18 09:06:09 +0200

    MovingZerosToTheEnd

---
+++ CodeWars.UnitTests/5kyu/MovingZerosToTheEndTests.cs
@@ -0,0 +1,14 @@
+﻿namespace CodeWars.UnitTests._5kyu
+{
+    public class MovingZerosToTheEndTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 }, new int[] { 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 })]
+        [InlineData(new int[] { 0, 0, 0 }, new int[] { 0, 0, 0 })]
+        [InlineData(new int[] { 0, 0, 0, 1 }, new int[] { 1, 0, 0, 0 })]
+        public void MoveZeroes(int[] arr, int[] expected)
+        {
+            Assert.Equal(expected, MovingZerosToTheEnd.MoveZeroes(arr));
+        }
+    }
+}
---
+++ CodeWars/5kyu/MovingZerosToTheEnd.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeWars.Core._5kyu
+{
+    public class MovingZerosToTheEnd
+    {
+        public static int[] MoveZeroes(int[] arr)
+        {
+            return arr.OrderBy(x => x == 0).ToArray();
+        }
+    }
+}
