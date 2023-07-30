550daf03b6d1cc5b7dee05b41a08d423e560f382
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-07-30 19:31:06 +0200

    updated global usings

--- CodeWars.UnitTests/Usings.cs
+++ CodeWars.UnitTests/Usings.cs
@@ -1,5 +1,6 @@
 global using Xunit;
 global using CodeWars.Core;
+global using CodeWars.Core._4kyu;
 global using CodeWars.Core._5kyu;
 global using CodeWars.Core._6kyu;
 global using CodeWars.Core._7kyu;
