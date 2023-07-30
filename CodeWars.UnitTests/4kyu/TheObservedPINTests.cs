f2fa161c4e529f398f8b727b4d81bb09ff3b5fcd
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-07-30 19:30:55 +0200

    The Observed PIN C#

---
+++ CodeWars.UnitTests/4kyu/TheObservedPINTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.UnitTests._4kyu
+{
+    public class TheObservedPINTests
+    {
+        [Theory]
+        [InlineData("8", new string[] { "5", "7", "8", "9", "0" })]
+        [InlineData("11", new string[] { "11", "22", "44", "12", "21", "14", "41", "24", "42" })]
+        [InlineData("369", new string[] { "339", "366", "399", "658", "636", "258", "268", "669", "668", "266", "369", "398", "256", "296", "259", "368", "638", "396", "238", "356", "659", "639", "666", "359", "336", "299", "338", "696", "269", "358", "656", "698", "699", "298", "236", "239" })]
+        public void GetPINsTest(string observed, string[] expected)
+        {
+            TheObservedPIN.GetPINs(observed).Should().BeEquivalentTo(expected);
+            //Assert.Equal(ex, );
+        }
+    }
+}
---
+++ CodeWars/4kyu/TheObservedPIN.cs
@@ -0,0 +1,48 @@
+﻿namespace CodeWars.Core._4kyu
+{
+    public class TheObservedPIN
+    {
+        public static List<string> GetPINs(string observed)
+        {
+            var list = new List<string>();
+            var dict = new Dictionary<string, List<string>>()
+            {
+                { "1", new List<string>() { "1", "2", "4" } },
+                { "2", new List<string>() { "1", "2", "3", "5" } },
+                { "3", new List<string>() { "2", "3", "6" } },
+                { "4", new List<string>() { "1", "4", "5", "7" } },
+                { "5", new List<string>() { "2", "4", "5", "6", "8" } },
+                { "6", new List<string>() { "3", "5", "6", "9" } },
+                { "7", new List<string>() { "4", "7", "8" } },
+                { "8", new List<string>() { "5", "7", "8", "9", "0" } },
+                { "9", new List<string>() { "6", "8", "9" } },
+                { "0", new List<string>() { "8", "0" } }
+            };
+            foreach (var item in observed)
+            {
+                list.Add(item.ToString());
+            }
+            return GetPermutations(list, dict);
+        }
+        private static List<string> GetPermutations(List<string> list, Dictionary<string, List<string>> dict)
+        {
+            var result = new List<string>();
+            if (list.Count == 1)
+            {
+                return dict[list[0]];
+            }
+            else
+            {
+                var temp = GetPermutations(list.Skip(1).ToList(), dict);
+                foreach (var item in dict[list[0]])
+                {
+                    foreach (var item2 in temp)
+                    {
+                        result.Add(item + item2);
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
