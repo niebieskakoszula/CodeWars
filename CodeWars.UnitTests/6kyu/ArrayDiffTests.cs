608748eca5e9613f1395b8f94d5a5302f908c958
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-07-31 10:02:52 +0200

    ArrayDiff

---
+++ CodeWars.UnitTests/6kyu/ArrayDiffTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.UnitTests._6kyu
+{
+    public class ArrayDiffTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2 }, new int[] { 1 }, new int[] { 2 })]
+        [InlineData(new int[] { 1, 2, 2 }, new int[] { 1 }, new int[] { 2, 2 })]
+        [InlineData(new int[] { 1, 2, 2 }, new int[] { 2 }, new int[] { 1 })]
+        [InlineData(new int[] { 1, 2, 2 }, new int[] { }, new int[] { 1, 2, 2 })]
+        [InlineData(new int[] { }, new int[] { 1, 2 }, new int[] { })]
+        public void Solve_ShouldReturnExpected(int[] a, int[] b, int[] expected)
+        {
+            // Arrange
+            // Act
+            var actual = ArrayDiff.Solve(a, b);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ CodeWars/6kyu/ArrayDiff.cs
@@ -0,0 +1,8 @@
+﻿namespace CodeWars.Core._6kyu
+{
+    public class ArrayDiff
+    {
+        public static int[] Solve(int[] a, int[] b) =>
+            a.Where(x => !b.Contains(x)).ToArray();
+    }
+}
