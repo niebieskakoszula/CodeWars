668f15ce32aaaa4e827acd03f3cac8a28a8e00df
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-07-31 10:47:21 +0200

    CountingDuplicates

---
+++ CodeWars.UnitTests/6kyu/CountingDuplicatesTests.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeWars.UnitTests._6kyu
+{
+    public class CountingDuplicatesTests
+    {
+        [Theory]
+        [InlineData("abcde", 0)]
+        [InlineData("abcdea", 1)]
+        [InlineData("indivisibility", 1)]
+        [InlineData("aabBcde", 2)]
+        [InlineData("Indivisibility", 1)]
+        [InlineData("Indivisibilities", 2)]
+        [InlineData("aAbBcC", 3)]
+        [InlineData("daAbBcC11", 4)]
+        public void DuplicateCount_ShouldReturnExpected(string str, int expected)
+        {
+            // Arrange
+            // Act
+            var actual = CountingDuplicates.DuplicateCount(str);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ CodeWars/6kyu/CountingDuplicates.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeWars.Core._6kyu
+{
+    public class CountingDuplicates
+    {
+        public static int DuplicateCount(string str)
+        {
+            return str.ToLower().GroupBy(c => c).Where(g => g.Count() > 1).Count();
+        }
+    }
+}
