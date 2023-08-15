341a8d312926d92b217918c49aa7f38347acef70
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-08-15 21:10:08 +0200

    RgbToHexConversion

---
+++ CodeWars.UnitTests/5kyu/RgbToHexConversionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.UnitTests._5kyu
+{
+    public class RgbToHexConversionTests
+    {
+        [Theory]
+        [InlineData(255, 255, 255, "FFFFFF")]
+        [InlineData(255, 255, 300, "FFFFFF")]
+        [InlineData(0, 0, 0, "000000")]
+        [InlineData(148, 0, 211, "9400D3")]
+        [InlineData(148, -20, 211, "9400D3")]
+        [InlineData(144, 195, 212, "90C3D4")]
+        [InlineData(212, 53, 12, "D4350C")]
+        public void RgbTest(int r, int g, int b, string expected)
+        {
+            Assert.Equal(expected, RgbToHexConversion.Rgb(r, g, b));
+        }
+    }
+}
---
+++ CodeWars/5kyu/RgbToHexConversion.cs
@@ -0,0 +1,20 @@
+﻿//https://www.codewars.com/kata/513e08acc600c94f01000001
+
+namespace CodeWars.Core._5kyu
+{
+    public class RgbToHexConversion
+    {
+        public static string Rgb(int r, int g, int b)
+        {
+            return $"{ToHex(r)}{ToHex(g)}{ToHex(b)}";
+        }
+        private static string ToHex(int value)
+        {
+            if (value < 0)
+                return "00";
+            if (value > 255)
+                return "FF";
+            return value.ToString("X2");
+        }
+    }
+}
