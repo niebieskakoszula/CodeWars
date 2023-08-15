e2a8a5516ccc29a435c932554f19a69950615839
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-08-15 21:09:23 +0200

    Intervals

---
+++ CodeWars.UnitTests/4kyu/IntervalsTests.cs
@@ -0,0 +1,49 @@
+﻿namespace CodeWars.UnitTests._4kyu
+{
+    public class IntervalsTests
+    {
+        [Fact]
+        public void ShouldHandleNullOrEmptyIntervals()
+        {
+            Assert.Equal(0, Intervals.SumIntervals(null));
+            Assert.Equal(0, Intervals.SumIntervals(new (int, int)[0]));
+        }
+        [Fact]
+        public void ShouldAddDisjoinedIntervals()
+        {
+            Assert.Equal(9, Intervals.SumIntervals(new (int, int)[] { (1, 2), (6, 10), (11, 15) }));
+            Assert.Equal(11, Intervals.SumIntervals(new (int, int)[] { (4, 8), (9, 10), (15, 21) }));
+            Assert.Equal(7, Intervals.SumIntervals(new (int, int)[] { (-1, 4), (-5, -3) }));
+            Assert.Equal(78, Intervals.SumIntervals(new (int, int)[] { (-245, -218), (-194, -179), (-155, -119) }));
+        }
+        [Fact]
+        public void ShouldAddAdjacentIntervals()
+        {
+            Assert.Equal(54, Intervals.SumIntervals(new (int, int)[] { (1, 2), (2, 6), (6, 55) }));
+            Assert.Equal(23, Intervals.SumIntervals(new (int, int)[] { (-2, -1), (-1, 0), (0, 21) }));
+        }
+
+        [Fact]
+        public void ShouldAddOverlappingIntervals()
+        {
+            Assert.Equal(7, Intervals.SumIntervals(new (int, int)[] { (1, 4), (7, 10), (3, 5) }));
+            Assert.Equal(6, Intervals.SumIntervals(new (int, int)[] { (5, 8), (3, 6), (1, 2) }));
+            Assert.Equal(19, Intervals.SumIntervals(new (int, int)[] { (1, 5), (10, 20), (1, 6), (16, 19), (5, 11) }));
+        }
+
+        [Fact]
+        public void ShouldHandleMixedIntervals()
+        {
+            Assert.Equal(13, Intervals.SumIntervals(new (int, int)[] { (2, 5), (-1, 2), (-40, -35), (6, 8) }));
+            Assert.Equal(1234, Intervals.SumIntervals(new (int, int)[] { (-7, 8), (-2, 10), (5, 15), (2000, 3150), (-5400, -5338) }));
+            Assert.Equal(158, Intervals.SumIntervals(new (int, int)[] { (-101, 24), (-35, 27), (27, 53), (-105, 20), (-36, 26) }));
+        }
+
+        [Fact]
+        public void ShouldHandleLargeIntervals()
+        {
+            Assert.Equal(2_000_000_000, Intervals.SumIntervals(new (int, int)[] { (-1_000_000_000, 1_000_000_000) }));
+            Assert.Equal(100_000_030, Intervals.SumIntervals(new (int, int)[] { (0, 20), (-100_000_000, 10), (30, 40) }));
+        }
+    }
+}
---
+++ CodeWars/4kyu/Intervals.cs
@@ -0,0 +1,32 @@
+﻿//https://www.codewars.com/kata/52b7ed099cdc285c300001cd/csharp
+
+namespace CodeWars.Core._4kyu
+{
+    public class Intervals
+    {
+        public static int SumIntervals((int, int)[] intervals)
+        {
+            if (intervals == null || intervals.Length == 0)
+                return 0;
+            var sortedIntervals = intervals.OrderBy(x => x.Item1).ToArray();
+            var result = 0;
+            var currentInterval = sortedIntervals[0];
+            for (var i = 1; i < sortedIntervals.Length; i++)
+            {
+                var nextInterval = sortedIntervals[i];
+                if (nextInterval.Item1 <= currentInterval.Item2)
+                {
+                    if (nextInterval.Item2 > currentInterval.Item2)
+                        currentInterval = (currentInterval.Item1, nextInterval.Item2);
+                }
+                else
+                {
+                    result += currentInterval.Item2 - currentInterval.Item1;
+                    currentInterval = nextInterval;
+                }
+            }
+            result += currentInterval.Item2 - currentInterval.Item1;
+            return result;
+        }
+    }
+}
